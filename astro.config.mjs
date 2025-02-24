2d54a53dce92cc12ea9afa1006203d847bd0e48f
DavidM004 <154438807+DavidM004@users.noreply.github.com>
2025-02-24 19:09:04 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node(),
   integrations: [tailwind(), alpinejs({ entrypoint: "/src/alpine" }), icon()],
   devToolbar: { enabled: false },
 });
